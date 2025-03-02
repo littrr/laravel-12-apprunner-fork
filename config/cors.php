352d3dc162bb9c6c63875f8afbaf54b9e274d7a4
Ntim Yeboah <lylegendary@gmail.com>
2025-03-02 10:06:20 +0000

    Force https scheme for production

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'public/build/*',],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
