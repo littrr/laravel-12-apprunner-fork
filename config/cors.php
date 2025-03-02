19ff59cfd951bd0abc69d3bca3a626a5929eaf29
Ntim Yeboah <lylegendary@gmail.com>
2025-03-02 09:09:25 +0000

    Fix public path in CSS CORS

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'build/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'public/*',],
 
     'allowed_methods' => ['*'],
 
