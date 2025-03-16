2901a1f927cb4530a697985f5ec360f096b3f57f
Ntim Yeboah <lylegendary@gmail.com>
2025-03-16 20:11:54 +0000

    Remove mocked health route

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/whats-your-health', function () {
-    return response()->json([
-        'message' => 'healthy',
-        'status' => 200,
-        'env' => [
-            'dbname' => getenv('dbname'),
-            'username' => getenv('username'),
-            'password' => getenv('password'),
-            'host' => getenv('host'),
-            'port' => getenv('port'),
-            'config database host' => config('database.connections.mysql.host'),
-        ]
-    ], 200);
-});
